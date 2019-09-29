b5d0e71471930e31dbc90acefba0b10a1d364893
Dawid J. Kubis <dawid.kubis3@gmail.com>
2019-09-29 13:37:47 +0200

    pridany komentare do skriptu

--- 1/kricici_lukas.py
+++ 1/kricici_lukas.py
@@ -1,7 +1,13 @@
 
+# ptame se uzivatel jestli lukas krici
 krici = input('Krici Lukas? : ')
+# promenna `krici` ma typ str
 
+# zjistujeme jestli uzivatel napsal ano
+# tady je rozdil mezi 'ano' a 'Ano' (velka pismena nemame osetrena)
 if krici == 'ano':
     print('Lukasi, mohl by ses trochu ztisit?')
 else:
     print('zrovna nemluvi?')
+
+# btw tohle vymyslel Lada
