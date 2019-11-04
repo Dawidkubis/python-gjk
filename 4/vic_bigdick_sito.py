46850067c9299ba42cde6e2ba09f74ba24b97255
lesves <lukas.veskrna@gmail.com>
2019-11-04 17:13:26 +0100

    Update vic_bigdick_sito.py

--- 4/vic_bigdick_sito.py
+++ 4/vic_bigdick_sito.py
@@ -6,3 +6,5 @@ from functools import reduce
 
 def primes(n):
     return reduce(lambda l, x: [y for y in l if y==x or y%x!=0], range(2, n), range(2, n))
+
+print(primes(int(input("Zadej cislo: "))))
